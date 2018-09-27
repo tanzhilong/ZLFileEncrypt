8cbba66fa3435373969b321e8c9f7aa3e15dc0bc
谭志龙 <tanzhilong2005@126.com>
2018-09-27 15:17:58 +0800

    Initial Commit

---
+++ ZLFileEncrypt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZLFileEncrypt
+//
+//  Created by 小陌雨 on 2018/9/27.
+//  Copyright © 2018年 小陌雨Moyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZLFileEncrypt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZLFileEncrypt
+//
+//  Created by 小陌雨 on 2018/9/27.
+//  Copyright © 2018年 小陌雨Moyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
